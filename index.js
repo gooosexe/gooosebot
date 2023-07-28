40327776ec7534664b6cef69a450a206f0fa6adc
Oliver Huang <olivershaohuang2006@gmail.com>
2023-07-28 00:02:07 -0400

    fixed token error

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // require discord.js classes
 const { Client, Events, GatewayIntentBits } = require('discord.js');
-const token = require('./config.json');
+const { token } = require('./config.json');
 
 // Create new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
